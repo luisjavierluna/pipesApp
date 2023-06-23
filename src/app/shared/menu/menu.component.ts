89acb01cdc3bdd0bba40d8dc2f00b9efe0ef3887
luisjavierluna <javierlunaluis@gmail.com>
2023-06-22 21:29:46 -0600

    10. PrimeNg - MenuBar

---
+++ src/app/shared/menu/menu.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html'
+})
+export class MenuComponent implements OnInit {
+
+  items: MenuItem[] = []
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.items = this.items = [{
+      label: 'File',
+      items: [
+        { label: 'New', icon: 'pi pi-plus' },
+        { label: 'Open', icon: 'pi pi-download' }
+      ]
+    },
+    {
+      label: 'Edit',
+      items: [
+        { label: 'Undo', icon: 'pi pi-refresh' },
+        { label: 'Redo', icon: 'pi pi-repeat' }
+      ]
+    }];
+  }
+
+}
