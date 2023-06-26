ad4001fac52bf2b5961d01c9c5f0b6b86eafd9b5
luisjavierluna <javierlunaluis@gmail.com>
2023-06-25 20:17:58 -0600

    21. I18nSelectPipe

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -6,11 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class NoComunesComponent implements OnInit {
+export class NoComunesComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  nombre: string = 'Luis'
+  genero: string = 'masculino'
+  
+  invitacionMapa = {
+    'masculino': 'invitarlo',
+    'femenino': 'invitarla'
   }
-
 }
