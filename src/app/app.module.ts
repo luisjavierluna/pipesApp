903515146b70c3aed281db2f3339a4ffd4722f38
luisjavierluna <javierlunaluis@gmail.com>
2023-06-28 15:14:20 -0600

    4. Pipe personalizado - mayusculasPipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import localeEs from '@angular/common/locales/es-MX'
 import localeFr from '@angular/common/locales/fr'
 import { registerLocaleData } from '@angular/common'
 registerLocaleData(localeEs)
+registerLocaleData(localeFr)
 
 @NgModule({
   declarations: [
