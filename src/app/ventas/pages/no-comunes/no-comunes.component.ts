78a49eed655d42a7e1934e5b4c232bf67b0ed8ca
luisjavierluna <javierlunaluis@gmail.com>
2023-06-28 13:43:37 -0600

    27. AsyncPipe

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-no-comunes',
@@ -40,17 +41,28 @@ export class NoComunesComponent {
   }
 
   heroes = [
-  {
-    nombre: 'Superman',
-    vuela: true
-  },
-  {
-    nombre: 'Robin',
-    vuela: false
-  },
-  {
-    nombre: 'Aquaman',
-    vuela: false
-  },
+    {
+      nombre: 'Superman',
+      vuela: true
+    },
+    {
+      nombre: 'Robin',
+      vuela: false
+    },
+    {
+      nombre: 'Aquaman',
+      vuela: false
+    },
   ]
+
+  // Async Pipe
+  miObservable = interval(5000) // 0, 1, 2, 3, 4, 5, 6
+
+  valorPromesa = new Promise((resolve, reject) => {
+
+    setTimeout(() => {
+      resolve( 'Tenemos data de promesa')
+    }, 3500);
+
+  })
 }
